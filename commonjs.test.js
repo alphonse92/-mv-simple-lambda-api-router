f2956811a0fde16d2d9ab4bbd2a8d6e54e7b614e
Eliecer Molina <eliecer.molina@uruit.com>
2023-04-04 17:12:17 -0500

    CHECKPOINT: adding module tests

--- commonjs.test.js
+++ commonjs.test.js
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const { AwsSamRouter, AwsProxyRouter } = require('./dist/classes');
+const { AwsSamRouter, AwsProxyRouter, ProxyRouter, BaseRouter } = require('./dist/classes');
 const AwsSamRouterClass = require('./dist/classes/AwsSamRouter');
-
 const AwsProxyRouterClass = require('./dist/classes/AwsProxyRouter');
+const ProxyRouterClass = require('./dist/classes/ProxyRouter');
+const BaseRouterClass = require('./dist/classes/BaseRouter');
 
 describe('CommonJS imports', () => {
   it('Should import AwsSamRouter', () => {
@@ -12,5 +13,11 @@ describe('CommonJS imports', () => {
 
   it('Should import AwsProxyRouter', () => {
     expect(AwsProxyRouter).toBe(AwsProxyRouterClass.default);
+  });
+  it('Should import ProxyRouter', () => {
+    expect(ProxyRouter).toBe(ProxyRouterClass.default);
+  });
+  it('Should import BaseRouter', () => {
+    expect(BaseRouter).toBe(BaseRouterClass.default);
   });
 });
--- tsmodule.test.ts
+++ tsmodule.test.ts
@@ -1,16 +1,32 @@
 import AwsSamRouterClass from './dist/classes/AwsSamRouter';
 import AwsProxyRouterClass from './dist/classes/AwsProxyRouter';
+import ProxyRouterClass from './dist/classes/ProxyRouter';
+import BaseRouterClass from './dist/classes/BaseRouter';
 
-import { AwsProxyRouter, AwsSamRouter } from './dist/classes';
+import { AwsProxyRouter, AwsSamRouter, ProxyRouter, BaseRouter } from './dist/classes';
 
 describe('Ts Modules', () => {
   it('Should import AwsSamRouter', () => {
     expect(AwsSamRouter).toBeDefined();
     expect(AwsSamRouterClass).toBeDefined();
+    expect(AwsSamRouterClass).toBe(AwsSamRouter);
   });
 
   it('Should import AwsProxyRouter', () => {
     expect(AwsProxyRouter).toBeDefined();
     expect(AwsProxyRouterClass).toBeDefined();
+    expect(AwsProxyRouterClass).toBe(AwsProxyRouter);
+  });
+
+  it('Should import ProxyRouter', () => {
+    expect(ProxyRouter).toBeDefined();
+    expect(ProxyRouterClass).toBeDefined();
+    expect(ProxyRouterClass).toBe(ProxyRouter);
+  });
+
+  it('Should import ProxyRouter', () => {
+    expect(BaseRouter).toBeDefined();
+    expect(BaseRouterClass).toBeDefined();
+    expect(BaseRouterClass).toBe(BaseRouter);
   });
 });
