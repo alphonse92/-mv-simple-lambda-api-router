7e2430ddc437bb3c8755e9f1c5dc562a9899197d
Eliecer Molina <eliecer.molina@uruit.com>
2023-04-04 17:06:04 -0500

    CHECKPOINT: exposing ProxyRouter

--- lib/classes/index.ts
+++ lib/classes/index.ts
@@ -1,5 +1,6 @@
 import BaseRouter from './BaseRouter';
 import AwsSamRouter from './AwsSamRouter';
 import AwsProxyRouter from './AwsProxyRouter';
+import ProxyRouter from './ProxyRouter';
 
-export { BaseRouter, AwsSamRouter, AwsProxyRouter };
+export { BaseRouter, AwsSamRouter, AwsProxyRouter, ProxyRouter };
