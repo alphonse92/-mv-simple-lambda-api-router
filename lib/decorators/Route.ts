9fed0624595eb5fa7aec5f874b44eb1d872acb1d
Eliecer Molina <eliecer.molina@uruit.com>
2023-04-05 11:06:44 -0500

    CHECKPOINT: lint

--- lib/decorators/Route.ts
+++ lib/decorators/Route.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export default function Route(method, path): any {
   return function RouteDecorator(target: any, propertyKey: any, descriptor: any): void {
     const decoredFunction = target[propertyKey];
