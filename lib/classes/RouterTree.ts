efddfb6ba5bb492249e365001010eb0e6fe9a38b
Eliecer Molina <eliecer.molina@uruit.com>
2023-04-06 15:49:55 -0500

    CHECKPOINT: fixtree router type

--- lib/classes/RouterTree.ts
+++ lib/classes/RouterTree.ts
@@ -3,7 +3,7 @@ import { RouterController } from '../types/TRouter';
 
 export type TreeRoutePath<T> = {
   route?: RouterController<T>;
-  childrenPaths?: TreeRoutePath<T>;
+  childrenPaths?: TreeRoutePath<T>[];
 };
 export default class RouterTree<T> implements IDataStructure<T> {
   get(index: IDataStructureIndexType): T {
